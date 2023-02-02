963cd3b87a4090a3cc25ab2380f944cf0a6a1550
Alexsander Gomes baptista <gomes.alexsander.hamir@outlook.com>
2023-02-02 13:13:41 -0800

    make it better

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <p className="footer__message">
           The best place to create great moments.
         </p>
-        <p className="">Contact us at <a target="_blank" href="mailto:socialtripmain@gmail.com">socialtripmain@gmail</a></p>
+        <p className="">Contact us at <a target="_blank" href="mailto:socialtripmain@gmail.com">socialtripmain@gmail.com</a></p>
         <p className="footer__message">
           Copyright © 2022 Social Trip All rights reserved.
         </p>
