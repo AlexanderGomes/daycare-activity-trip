ff88f05075feb00d6140c2c57c2a94f7c656130a
Alexsander Gomes baptista <gomes.alexsander.hamir@outlook.com>
2023-02-01 19:39:02 -0800

    make it better

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
+ require("dotenv").config();
+ const path = require("path");
 
 const port = process.env.PORT || 5000;
 const dbConnect = require("./utils/dbConnect");
@@ -20,6 +21,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/user", UserRoutes);
 app.use("/api/trip", TripRoutes);
 app.use("/api/temp", TempRoutes);
+
+
+//Server frontend
+if (process.env.NODE__ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 app.listen(port, async () => {
   await dbConnect();
